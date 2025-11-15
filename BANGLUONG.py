e0777363bc85a6020d9541b5d7ee0daa2c1da18a
datvtm67khmt-svg <dat.vtm.67khmt@ntu.edu.vn>
2025-11-15 15:15:04 +0700

    Create BANGLUONG.py

---
+++ BANGLUONG.py
@@ -0,0 +1,9 @@
+Ten1 = "Lê Mai Anh"
+Cv1 = "Nhân viên"
+Th1 = 500000
+Ten2 = "Trần Minh Nguyên"
+Cv2 = "Giám đốc chi nhánh"
+Th2 = 10000000
+print("{0:<22}{1:<22}{2:>22}".format("Họ tên", "Chức vụ", "Thưởng"))
+print(f"{Ten1:<20}{Cv1:<20}{Th1:>10}")
+print(f"{Ten2:<20}{Cv2:<20}{Th2:>10}")
